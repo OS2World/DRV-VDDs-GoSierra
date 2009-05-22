db314ea1e1bc3345ec251968fd763a52f812e6f5
m_kiewitz <m_kiewitz@675af19a-70d5-443c-a3d9-9246a67bb258>
2009-05-22 22:43:12 +0000

    vCOMPAT.031 directory removed

--- VDDs/vCOMPAT.031/MagicVMP.h
+++
@@ -1,6 +0,0 @@
-
-void VCOMPAT_MagicVMPatcherInRM_TurboPascalCRT    (PCRF pcrf);
-void VCOMPAT_MagicVMPatcherInRM_MS_C_TimerInitBug (PCRF pcrf);
-void VCOMPAT_MagicVMPatcherInPM_INT10             (PCRF pcrf);
-void VCOMPAT_MagicVMPatcherInPM_GetDPMIVersion    (PCRF pcrf);
-void VCOMPAT_AutoVPMSti                           (PVOID p, PCRF pcrf);
